5f09d95d2ecf2de4b7056ac58625c72cb93032cd
Shawna Hightower <sdhightower6@gmail.com>
2022-03-06 19:16:59 -0700

    troubleshooting

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -9,14 +10,14 @@ def index():
     if request.method == "POST":
         login = request.form['login']
 
-    github_url = requests.get(
-        'https://api.github.com/users/{login}')
+        github_url = requests.get(
+            f'https://api.github.com/users/{login}')
 
-    userData = github_url.json()
+        userData = github_url.json()
 
-    avatar_url = (userData['avatar_url'])
-    bio = (userData['bio'])
-    name = (userData['name'])
+        avatar_url = (userData['avatar_url'])
+        bio = (userData['bio'])
+        name = (userData['name'])
 
     return render_template('index.html', avatar_url=avatar_url, bio=bio, name=name, login=login)
 
