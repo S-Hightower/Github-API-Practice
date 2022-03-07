77d68985b5379c47e3339caffbd9c7282aa16827
Shawna Hightower <sdhightower6@gmail.com>
2022-03-06 18:55:39 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == "POST":
+        login = request.form['login']
+
+    github_url = requests.get(
+        'https://api.github.com/users/{login}')
+
+    userData = github_url.json()
+
+    avatar_url = (userData['avatar_url'])
+    bio = (userData['bio'])
+    name = (userData['name'])
+
+    return render_template('index.html', avatar_url=avatar_url, bio=bio, name=name, login=login)
+
+if __name__=="__main__":
+    app.run(debug=True)
